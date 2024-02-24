f5b0f9eda2cef7008553ddff3fda4b014068a206
unknown <s510409@yahoo.com.tw>
2024-02-24 18:04:41 +0800

    2024-02-24 ver1

---
+++ #122.Best_Time_to_Buy_and_Sell_Stock_II-Python.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def maxProfit(self, prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    max_profit = 0
+    for i in range(1,len(prices)):
+        if prices[i]>prices[i-1]:
+            max_profit = max_profit + (prices[i]-prices[i-1])
+    return max_profit
+
